013f4fbd2b3e74c87fa69f44c47521f64a1c09b1
Sergio Vela Pelegrina <sergio.vela@liones.nl>
2022-08-28 12:42:18 +0200

    Simulate we know the number of favorite items to display

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,17 +1,15 @@
-import MeetupItem from "../components/meetups/MeetupItem";
-import classes from "./../components/meetups/MeetupList.module.css";
-
+import MeetupItem from '../components/meetups/MeetupItem';
+import classes from './../components/meetups/MeetupList.module.css';
 
 export default function AllMeetupsPage() {
-  return (
-    <section>
-      <h1>All Meetups</h1>
-      <ul className={classes.list}>
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
-      </ul>
-    </section>
-  );
+    return (
+        <section>
+            <h1>All Meetups</h1>
+            <ul className={classes.list}>
+                {[...Array(7).keys()].map((_, index) => {
+                    return <MeetupItem key={index} id={index} />;
+                })}
+            </ul>
+        </section>
+    );
 }
