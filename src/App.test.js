c83b1b4c97e91bd050b8f00b0778641147477c0a
Sergio Vela Pelegrina <sergio.vela@liones.nl>
2022-08-28 12:32:16 +0200

    Remove an unnecessary test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 /* eslint-disable testing-library/await-async-query */
 /* eslint-disable testing-library/no-debugging-utils */
-import { shallow, mount } from "enzyme";
-import App from "./App";
-import MainNavigation from "./components/layout/MainNavigation";
-import Layout from "./components/layout/Layout";
+import { shallow } from 'enzyme';
+import App from './App';
+import Layout from './components/layout/Layout';
 
 /**
  * Factory funcion to create a ShallowWrapper for the App component
@@ -11,20 +10,14 @@ import Layout from "./components/layout/Layout";
  * @returns {ShallowWrapper}
  */
 const setup = () => shallow(<App />);
-const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test]='${val}'`);
 
-test("renders App without crashing", () => {
-  const wrapper = setup();
-  //console.log(wrapper.debug());
-  expect(wrapper.exists()).toBe(true);
+test('renders App without crashing', () => {
+    const wrapper = setup();
+    //console.log(wrapper.debug());
+    expect(wrapper.exists()).toBe(true);
 });
 
-test("renders the navigation component", () => {
-  const wrapper = setup();
-  expect(wrapper.find(MainNavigation).length).toBe(1);
-});
-
-test("renders the Layout component", () => {
-  const wrapper = setup();
-  expect(wrapper.find(Layout).length).toBe(1);
+test('renders the Layout component', () => {
+    const wrapper = setup();
+    expect(wrapper.find(Layout).length).toBe(1);
 });
