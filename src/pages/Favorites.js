3ad1ca2cba4cdd14f42e4ab822d252d245757bce
Sergio Vela Pelegrina <sergio.vela@liones.nl>
2022-08-28 12:41:30 +0200

    Display favorites meetups

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,24 @@
+import MeetupItem from '../components/meetups/MeetupItem';
+import getFavorites from '../utils/getFavorites';
+import classes from './../components/meetups/MeetupList.module.css';
+
 export default function FavoritesPage() {
-  return (
-    <section>
-      <h1>Favorites Page</h1>
-    </section>
-  );
+    const favoritesMeetupts = getFavorites();
+
+    return (
+        <section>
+            <h1>Favorites Page</h1>
+            <ul className={classes.list}>
+                {favoritesMeetupts.map((_, index) => {
+                    return (
+                        <MeetupItem
+                            key={index}
+                            id={index}
+                            isFavoritePage={true}
+                        />
+                    );
+                })}
+            </ul>
+        </section>
+    );
 }
