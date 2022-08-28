16668d5fbcfbcc56592a64904cc0fb50091e4aeb
Sergio Vela Pelegrina <sergio.vela@liones.nl>
2022-08-28 12:35:29 +0200

    Clean up the file and include react-router-dom module

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import { useState } from "react";
+import AllMeetupsPage from './pages/AllMeetupsPage';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetup';
 
-import AllMeetupsPage from "./pages/AllMeetupsPage";
-import FavoritesPage from "./pages/Favorites";
-import NewMeetupsPage from "./pages/NewMeetup";
-import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./utils/constants";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import MainNavigation from "./components/layout/MainNavigation";
-import Layout from "./components/layout/Layout";
+import Layout from './components/layout/Layout';
 
 function App() {
-  const [page, setPage] = useState(ALL_MEETUP_PAGE);
-
-  function getCurrentPageComponent() {
-    let currentPageComponent = <AllMeetupsPage />;
-    switch (page) {
-      case FAVORITES_PAGE:
-        currentPageComponent = <FavoritesPage />;
-        break;
-      case NEW_MEETUP_PAGE:
-        currentPageComponent = <NewMeetupsPage />;
-        break;
-      default:
-        currentPageComponent = <AllMeetupsPage />;
-    }
-
-    return currentPageComponent;
-  }
-
-  return (
-    <div data-test="app">
-      <MainNavigation setPage={setPage} />
-      <Layout>{getCurrentPageComponent()}</Layout>
-    </div>
-  );
+    return (
+        <Router>
+            <div data-test="app">
+                <Layout>
+                    <Routes>
+                        <Route exact path={'/'} element={<AllMeetupsPage />} />
+                        <Route
+                            exact
+                            path={'/favorites'}
+                            element={<FavoritesPage />}
+                        />
+                        <Route
+                            exact
+                            path={'/favorites'}
+                            element={<FavoritesPage />}
+                        />
+                        <Route
+                            exact
+                            path={'/new'}
+                            element={<NewMeetupsPage />}
+                        />
+                    </Routes>
+                </Layout>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
